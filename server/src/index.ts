3e73d2b8948a711174792e7ff76f7091dc470654
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-26 16:11:12 +0530

    Implemented graceful shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,3 +15,32 @@ const server = app.listen(PORT, async () => {
   // await connect();
   // routes(app);
 });
+
+const SIGNALS = ["SIGINT", "SIGTERM"];
+
+const gracefulshutdown = (signal: string) => {
+  process.once(signal, async () => {
+    console.log(`Recieved ${signal}, implementing graceful shutdown...`);
+    try {
+      await Promise.all([
+        new Promise((reject, resolve) => {
+          server.close((err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        }),
+        // disconnect();
+      ]);
+      console.log(`Shutdown Completed`);
+    } catch (error: any) {
+      console.log(`Something went wrong: `, error.message);
+    }
+  });
+};
+
+for (const SIGNAL in SIGNALS) {
+  // gracefulshutdown(SIGNAL);
+}
