f5f8587fae03b4e67f312117a012e9c19b8f4d5e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-27 11:12:23 +0530

    Implemented an JWT Utility class to generate and verify tokens

--- server/src/modules/auth/auth.utils.ts
+++ server/src/modules/auth/auth.utils.ts
@@ -1,13 +1,59 @@
-import Jwt, { SignOptions } from "jsonwebtoken";
+import jwt, {
+  JsonWebTokenError,
+  NotBeforeError,
+  TokenExpiredError,
+} from "jsonwebtoken";
+import { privateKey, publicKey } from "../../utils/pemToString";
 
-export const signJwt = (
-  payload: Buffer | object | string,
-  options?: SignOptions
-) => {
-  const expiresIn = process.env.EXPIRES_IN;
-  return Jwt.sign(payload, "signingKey", {
-    ...(options && options),
-    algorithm: "RS256",
-    expiresIn: expiresIn,
-  });
-};
+type JwtPayload = object | Buffer | string;
+
+export class JWTService {
+  private readonly privateKey: Buffer;
+  private readonly publicKey: Buffer;
+
+  constructor() {
+    try {
+      // Load the keys from files and store them in memory
+      this.privateKey = privateKey;
+      this.publicKey = publicKey;
+    } catch (error: any) {
+      throw new Error(
+        `Failed to load JWT key files: ${error.message}. \nPlease generate public and private key pairs.`
+      );
+    }
+  }
+
+  generateToken(payload: JwtPayload): string {
+    try {
+      const EXPIRES_IN = process.env.EXPIRES_IN;
+      const options: jwt.SignOptions = {
+        algorithm: "RS256",
+        expiresIn: EXPIRES_IN,
+      };
+      return jwt.sign(payload, this.privateKey, options);
+    } catch (err) {
+      throw new Error("Failed to generate JWT token.");
+    }
+  }
+
+  verifyToken(token: string): JwtPayload {
+    try {
+      const options: jwt.VerifyOptions = {
+        algorithms: ["RS256"],
+      };
+      return jwt.verify(token, this.publicKey, options) as JwtPayload;
+    } catch (err) {
+      if (err instanceof JsonWebTokenError) {
+        throw new Error("Invalid JWT token.");
+      } else if (err instanceof NotBeforeError) {
+        throw new Error("JWT token not yet valid.");
+      } else if (err instanceof TokenExpiredError) {
+        throw new Error("JWT token has expired.");
+      } else {
+        throw new Error("Failed to verify JWT token.");
+      }
+    }
+  }
+}
+
+export default JWTService;
