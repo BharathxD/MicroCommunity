1452995bce6c4ed1beda927b1a8a6137bf082a1e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-29 09:06:40 +0530

    Cleanup server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,32 @@
 import express from "express";
-import dotenv from "dotenv";
 import logger from "./utils/logger";
-import { connect } from "./utils/connect";
 import { configureApp } from "./config/config";
+import { connect } from "./utils/connect";
 import routes from "./routes";
 import gracefulShutdown from "./config/gracefulShutdown";
-
+import dotenv from "dotenv";
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
 
-/* CONFIGURATION */
+/**
+ * Server Configuration
+ */
+
 configureApp(app);
 
 const server = app.listen(PORT, async () => {
   logger.info(`The server is running at http://localhost:${PORT}`);
   //? Connect to the Database
   await connect();
-  //? Routes
+  //? Handle Routes
   routes(app);
 });
+
+/**
+ * Implement Graceful Shutdown
+ */
 
 const SIGNALS = ["SIGTERM", "SIGINT"];
 
