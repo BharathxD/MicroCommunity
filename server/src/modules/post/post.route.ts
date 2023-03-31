fed3e78a58cf6463cedd96c0b678442b309daa1f
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-31 08:37:50 +0530

    Added a middleware to verify the user token

--- server/src/modules/post/post.route.ts
+++ server/src/modules/post/post.route.ts
@@ -3,16 +3,23 @@ import { createPostHandler, likePostHandler } from "./post.controller";
 import upload from "../../utils/fileUpload";
 import { LikePostSchema, PostSchema } from "./post.schema";
 import { processRequestBody } from "zod-express-middleware";
+import requireUser from "../../middlewares/requireUser";
 
 const router = express.Router();
 
 router.post(
   "/",
+  requireUser,
   processRequestBody(PostSchema.body),
   upload.single("picture"),
   createPostHandler
 );
 
-router.patch("/", processRequestBody(LikePostSchema.body), likePostHandler);
+router.patch(
+  "/",
+  requireUser,
+  processRequestBody(LikePostSchema.body),
+  likePostHandler
+);
 
 export default router;
