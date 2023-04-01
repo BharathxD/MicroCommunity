75bde8235270bef67e86e204bc14deaeaa49da31
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-01 12:03:48 +0530

    Created and Tested connections handler

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from "http-status-codes";
 import { createUser, findUserById } from "./user.service";
 import { RegisterInput } from "./user.schema";
 import logger from "../../utils/logger";
+import { omit } from "lodash";
 
 export const registerUserHandler = async (
   req: Request<{}, {}, RegisterInput>,
@@ -47,12 +48,13 @@ export const registerUserHandler = async (
 
 export const getUserHandler = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params;
-    const user = await findUserById(id);
+    const { userId } = req.params;
+    const user = await findUserById(userId);
     if (!user) {
       throw new Error("User not found");
     }
-    res.status(200).json(user);
+    const transformedUser = omit(user.toJSON(), "password");
+    res.status(StatusCodes.OK).json(transformedUser);
   } catch (error: any) {
     res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
   }
@@ -63,17 +65,21 @@ export const getUserConnectionsHandler = async (
   res: Response
 ) => {
   try {
-    const { id } = req.params;
-    const user = await findUserById(id);
+    const userId = res.locals.user._id;
+    const user = await findUserById(userId);
 
     if (!user) {
-      throw new Error("User not found");
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "User not Found" });
     }
 
     const connections = user.connections;
 
-    if (!connections) {
-      throw new Error("No connections found");
+    if (!connections || connections.length === 0) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "No connections Found" });
     }
 
     const friends = await Promise.all(
@@ -91,7 +97,33 @@ export const getUserConnectionsHandler = async (
       .status(StatusCodes.OK)
       .json(formattedFriends.filter((friend) => friend !== null));
   } catch (error: any) {
-    logger.error(error);
-    res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
+    logger.error(error.message);
+  }
+};
+
+export const connectionHandler = async (req: Request, res: Response) => {
+  try {
+    const { connectionId } = req.params;
+    const userId = res.locals.user._id;
+    const user = await findUserById(userId);
+    const connection = await findUserById(connectionId);
+    if (!connection || !user) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .send({ message: "Connection or User not found" });
+    }
+    if (user.connections?.includes(connectionId)) {
+      user.connections = user.connections?.filter((id) => id !== connectionId);
+      connection.connections = connection.connections?.filter(
+        (id) => id !== userId
+      );
+    } else {
+      user.connections?.push(connectionId);
+      connection.connections?.push(userId);
+    }
+    Promise.all([await user.save(), await connection.save()]);
+    res.status(StatusCodes.OK).send({ message: "Ok" });
+  } catch (error: any) {
+    logger.error(`Something went wrong: ${error.message}`);
   }
 };
--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { registerUserHandler } from "./user.controller";
+import {
+  connectionHandler,
+  getUserConnectionsHandler,
+  getUserHandler,
+  registerUserHandler,
+} from "./user.controller";
 import upload from "../../utils/fileUpload";
 import { processRequestBody } from "zod-express-middleware";
 import { RegisterSchema } from "./user.schema";
+import requireUser from "../../middlewares/requireUser";
 
 const router = Router();
 
@@ -12,5 +18,11 @@ router.post(
   upload.single("picture"),
   registerUserHandler
 );
+
+router.get("/search/:userId", requireUser, getUserHandler);
+
+router.get("/connections", requireUser, getUserConnectionsHandler);
+
+router.get("/connections/:connectionId", requireUser, connectionHandler);
 
 export default router;
