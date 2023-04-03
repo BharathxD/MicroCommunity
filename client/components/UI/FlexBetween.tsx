4ae492bf792cdb72a16a5f7fcc247fd3e285baec
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-03 09:34:40 +0530

    Feat: Create FlexBetween styled component for MUI Box with flexbox properties

---
+++ client/components/UI/FlexBetween.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
