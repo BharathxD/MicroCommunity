3584fc5eec6884d121ebf69c98728b374daf6b73
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-03 20:50:04 +0530

    Implemented Register api

--- client/api/index.tsx
+++ client/api/index.tsx
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const base = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
-const loginBase = `${base}/api/auth`;
+const authBase = `${base}/api/auth`;
+const userBase = `${base}/api/user`;
 
 export const loginUser = async (payload: {
   username: string;
   password: string;
 }) => {
   try {
-    const response = await axios.post(loginBase, payload, {
+    const response = await axios.post(authBase, payload, {
       withCredentials: true,
     });
     const data = await response.data;
@@ -17,5 +18,28 @@ export const loginUser = async (payload: {
   } catch (error: any) {
     console.log(error.message);
     return null;
+  }
+};
+
+export const registerUser = async (payload: {
+  fname: string;
+  lname: string;
+  occupation: string;
+  location: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}) => {
+  try {
+    const response = await axios.post(userBase, payload, {
+      withCredentials: true,
+    });
+    const data = await response.data;
+    return {
+      userIsCreated: response.status === 200,
+      data,
+    };
+  } catch (error: any) {
+    console.log(error.message);
   }
 };
