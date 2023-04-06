db16319172eb491f5b401d07ab8851bffadf9ff1
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-06 13:59:14 +0530

    Refactor and refine HomePageLayout component

--- client/layout/HomePageLayout.tsx
+++ client/layout/HomePageLayout.tsx
@@ -1,30 +1,33 @@
 import { AppShell } from "@mantine/core";
 import React, { useMemo } from "react";
-import Header from "./Header";
 import { useSelector } from "react-redux";
+import { ThemeProvider, createTheme } from "@mui/material";
 import { ReduxState } from "@/types/state.types";
 import { themeSettings } from "@/themes/theme";
-import { Box, ThemeProvider, createTheme } from "@mui/material";
 
-const HomePageLayout = ({
-  children,
-  withoutHeader,
-}: {
+import Header from "./Header";
+
+interface Props {
   children: React.ReactNode;
   withoutHeader?: boolean;
+}
+
+const HomePageLayout: React.FC<Props> = ({
+  children,
+  withoutHeader = false,
 }) => {
   const mode = useSelector((state: ReduxState) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return (
-    <ThemeProvider theme={theme}>
-      {!withoutHeader && (
-        <AppShell padding={"md"} header={<Header />}>
-          {children}
-        </AppShell>
-      )}
-      {withoutHeader && <>{children}</>}
-    </ThemeProvider>
+
+  const content = withoutHeader ? (
+    children
+  ) : (
+    <AppShell padding="md" header={<Header />}>
+      {children}
+    </AppShell>
   );
+
+  return <ThemeProvider theme={theme}>{content}</ThemeProvider>;
 };
 
 export default HomePageLayout;
