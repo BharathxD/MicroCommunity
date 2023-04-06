79db9b06ecb0951129d0da03734f1eaf6706e260
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-06 14:24:51 +0530

    Refactor and refine RegisterUser component and its dependencies using MUI sx prop for styling and optimize media query logic.

--- client/pages/auth/index.tsx
+++ client/pages/auth/index.tsx
@@ -1,34 +1,41 @@
-import React, { ReactElement } from "react";
-import { useMediaQuery } from "@mui/material";
-import { Box } from "@mui/material";
+import React from "react";
+import { useMediaQuery, Box } from "@mui/material";
 import { useTheme } from "@mui/material";
 import HomePageLayout from "@/layout/HomePageLayout";
-import Form from "@/components/Form/Form";
+import Form from "@/components/Form";
 import Logo from "@/layout/Header/Logo";
+import { ReactElement } from "react";
 
 const RegisterUser = () => {
   const theme = useTheme();
   const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  const boxWidth = isNonMobileScreen ? "40%" : "100%";
+  const boxPadding = isNonMobileScreen ? "2rem" : "1rem";
+  const bgColor = theme.palette.background.alt;
+
   return (
     <Box
-      width="100%"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      bgcolor={theme.palette.background.alt}
-      textAlign="center"
-      height="100vh"
-      p="15px"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        height: "100vh",
+        p: "15px",
+        bgcolor: { bgColor },
+      }}
     >
       <Logo />
       <Box
-        width={isNonMobileScreen ? "40%" : "100%"}
-        p={isNonMobileScreen ? "2rem" : "1rem"}
-        m="0.25rem"
-        borderRadius="1.5rem"
-        flexDirection="column"
-        bgcolor={theme.palette.background.alt}
+        sx={{
+          width: boxWidth,
+          p: boxPadding,
+          m: "0.25rem",
+          borderRadius: "1.5rem",
+          bgcolor: { bgColor },
+        }}
       >
         <Form />
       </Box>
@@ -36,7 +43,7 @@ const RegisterUser = () => {
   );
 };
 
-RegisterUser.getLayout = function (page: ReactElement) {
+RegisterUser.getLayout = function getLayout(page: ReactElement) {
   return <HomePageLayout withoutHeader>{page}</HomePageLayout>;
 };
 
