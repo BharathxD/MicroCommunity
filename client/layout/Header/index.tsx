d38ff2379109d6ffc163e60b85cad097dcb511af
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-06 14:04:20 +0530

    FEAT: Refactor and refine Header component

--- client/layout/Header/index.tsx
+++ client/layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 import Search from "./Search";
 import { IconButton, useTheme, useMediaQuery } from "@mui/material";
 import { Menu } from "@mui/icons-material";
@@ -10,32 +11,38 @@ import MobileNav from "./MobileNav";
 const Header = () => {
   const [isMobileMenuToggled, setMobileMenuToggled] = useState<boolean>();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-  const theme = useTheme();
-  const alt = theme.palette.background.alt;
+  const {
+    palette: {
+      background: { alt },
+    },
+  } = useTheme();
 
   return (
-    <>
+    <React.Fragment>
       <FlexBetween padding="1rem 6%" bgcolor={alt}>
         <FlexBetween gap="1.75rem">
           <Logo />
           {isNonMobileScreens && <Search />}
         </FlexBetween>
-        {isNonMobileScreens && <DesktopNav />}
-        {!isNonMobileScreens && (
-          <IconButton
-            onClick={() => setMobileMenuToggled(!isMobileMenuToggled)}
-          >
-            <Menu />
-          </IconButton>
-        )}
-        {!isNonMobileScreens && isMobileMenuToggled && (
-          <MobileNav
-            setMobileMenuToggled={setMobileMenuToggled}
-            isMobileMenuToggled={isMobileMenuToggled}
-          />
+        {isNonMobileScreens ? (
+          <DesktopNav />
+        ) : (
+          <React.Fragment>
+            <IconButton
+              onClick={() => setMobileMenuToggled(!isMobileMenuToggled)}
+            >
+              <Menu />
+            </IconButton>
+            {isMobileMenuToggled && (
+              <MobileNav
+                setMobileMenuToggled={setMobileMenuToggled}
+                isMobileMenuToggled={isMobileMenuToggled}
+              />
+            )}
+          </React.Fragment>
         )}
       </FlexBetween>
-    </>
+    </React.Fragment>
   );
 };
 
