b9bdd8f3c61f850359577774276750cd0b154c9f
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-07 08:52:42 +0530

    Configure connections component

---
+++ client/components/widget/Connections.tsx
@@ -0,0 +1,9 @@
+import { Box, useTheme } from "@mui/material";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { ReduxState } from "@/types/state.types";
+import { useDispatch } from "react-redux";
+
+const Friend = () => {};
+
+export default Friend;
