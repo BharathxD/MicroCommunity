ed4cca14d1a6244a5cb4471a3744840b01a63325
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-07 08:44:19 +0530

    Added a component to show user's profile picture

--- client/components/widget/UserImage.tsx
+++ client/components/widget/UserImage.tsx
@@ -1,5 +1,22 @@
-const UserImage = ({}) => {
+import { Box } from "@mui/material";
+import Image from "next/image";
 
+type Props = {
+  image: string;
+  size: number;
+};
+const UserImage = ({ image, size = 60 }: Props) => {
+  return (
+    <Box width={size} height={size}>
+      <Image
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={`http://localhost:3001/assets/${image}`}
+      />
+    </Box>
+  );
 };
 
 export default UserImage;
