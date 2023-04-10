0f5db72df304d3db833a934d0835f7eb45535a83
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-04-10 18:04:10 +0530

    Refactor and refine code for RegisterUser component for improved readability and maintainability

--- client/pages/auth/index.tsx
+++ client/pages/auth/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from "react";
-import { useMediaQuery } from "@mui/material";
+import { useMediaQuery, useTheme } from "@mui/material";
 import { Box } from "@mui/material";
-import { useTheme } from "@mui/material";
 import HomePageLayout from "@/layout/HomePageLayout";
 import Form from "@/components/Form";
 import Logo from "@/layout/Header/Logo";
@@ -9,26 +8,33 @@ import Logo from "@/layout/Header/Logo";
 const RegisterUser = () => {
   const theme = useTheme();
   const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const formContainerWidth = isNonMobileScreen ? "40%" : "100%";
+  const formContainerPadding = isNonMobileScreen ? "2rem" : "1rem";
+
   return (
     <Box
-      width="100%"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      bgcolor={theme.palette.background.alt}
-      textAlign="center"
-      height="100vh"
-      p="15px"
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        bgcolor: theme.palette.background.alt,
+        textAlign: "center",
+        height: "100vh",
+        p: "15px",
+      }}
     >
       <Logo />
       <Box
-        width={isNonMobileScreen ? "40%" : "100%"}
-        p={isNonMobileScreen ? "2rem" : "1rem"}
-        m="0.25rem"
-        borderRadius="1.5rem"
-        flexDirection="column"
-        bgcolor={theme.palette.background.alt}
+        sx={{
+          width: formContainerWidth,
+          p: formContainerPadding,
+          m: "0.25rem",
+          borderRadius: "1.5rem",
+          flexDirection: "column",
+          bgcolor: theme.palette.background.alt,
+        }}
       >
         <Form />
       </Box>
