c12044dbbeae0ef2a93f84b68cd02fdc7812cf1d
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-01 20:18:06 +0530

    Ensures that the server uses a default port

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 //? Server Configuration
 configureApp(app);
