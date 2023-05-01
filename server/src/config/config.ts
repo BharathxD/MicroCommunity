40c5820c20560831098e71b209616afbf114560f
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-01 20:20:05 +0530

    Refactor config.ts

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -7,18 +7,22 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 export const configureApp = (app: Express) => {
-  const origin1 = process.env.CORS_ORIGIN || "";
+  const corsOrigin = process.env.CORS_ORIGIN || "";
+
   app.use(cookieParser());
   app.use(express.json());
+  app.use(helmet());
+  app.use(deserializeUser);
+
+  const publicPath = path.join(__dirname, "..", "..", "public/assets");
+  app.use("/public", express.static(publicPath));
+
   app.use(
     cors({
-      origin: ["http://192.168.52.201:3000", origin1],
+      origin: corsOrigin,
       credentials: true,
       methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     })
   );
-  app.use(helmet());
-  app.use(deserializeUser);
-  const publicPath = path.join(__dirname, "..", "..", "public/assets");
-  app.use("/public", express.static(publicPath));
 };
+
