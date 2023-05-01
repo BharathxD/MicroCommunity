f86aee2b23daa6604103b82b27edb5e5851c61f3
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-01 20:16:42 +0530

    Added useful comments

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,18 +7,21 @@ import PostRoute from "./modules/post/post.route";
 const apiPrefix = "/api";
 
 const routes = (app: Express) => {
+  //? Route to Check Server Status
   app.get("/", (_, res: Response) => {
     res
       .status(StatusCodes.OK)
       .send({ message: "The Server is up and running" });
   });
-  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Internal Server Error" });
-  });
+  //? API Routes
   app.use(`${apiPrefix}/user`, UserRoute);
   app.use(`${apiPrefix}/auth`, AuthRoute);
   app.use(`${apiPrefix}/post`, PostRoute);
+  //? Error Handling
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err.stack);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Internal Server Error" });
+  });
 };
 
 export default routes;
