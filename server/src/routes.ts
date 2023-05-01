ecf4539eecdf843c49251339881d1410f3ae60bb
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-01 20:12:43 +0530

    Use common API Prefix

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,15 +4,17 @@ import UserRoute from "../src/modules/user/user.route";
 import AuthRoute from "../src/modules/auth/auth.route";
 import PostRoute from "./modules/post/post.route";
 
+const apiPrefix = "/api";
+
 const routes = (app: Express) => {
   app.get("/", (_, res: Response) => {
     res
       .status(StatusCodes.OK)
       .send({ message: "The Server is up and running" });
   });
-  app.use("/api/user", UserRoute);
-  app.use("/api/auth", AuthRoute);
-  app.use("/api/post", PostRoute);
+  app.use(`${apiPrefix}/user`, UserRoute);
+  app.use(`${apiPrefix}/auth`, AuthRoute);
+  app.use(`${apiPrefix}/post`, PostRoute);
 };
 
 export default routes;
